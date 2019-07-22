421d86f35ccbaff97d3a5e437eb24af5f2219140
Mark W <mark.wilkins@uberflip.com>
2019-07-22 19:44:26 -0400

    WIP

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import knex from 'knex';
 import router from './routes';
+
+const knexInstance: knex = knex({});
 
 const app: express.Application = express();
 const port: number = process.env.PORT ? parseInt(<string>process.env.PORT) : 3000;
